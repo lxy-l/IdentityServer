72d90378b1946a86d4b5a70745eb41ac1c7e10f4
Roy <lxysac@foxmail.com>
2022-12-06 13:18:30 +0800

    .

--- OpenIddictServer/Worker.cs
+++ OpenIddictServer/Worker.cs
@@ -1,9 +1,6 @@
 ﻿using OpenIddict.Abstractions;
-using static OpenIddict.Abstractions.OpenIddictConstants;
-using System.Globalization;
+
 using OpenIddictServer.Data;
-using static OpenIddict.Client.WebIntegration.OpenIddictClientWebIntegrationConstants;
-using Microsoft.EntityFrameworkCore;
 
 namespace OpenIddictServer
 {
@@ -18,118 +15,22 @@ namespace OpenIddictServer
         {
             using var scope = _serviceProvider.CreateScope();
 
-            var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            await context.Database.EnsureCreatedAsync(cancellationToken);
+            using var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await context.Database.EnsureCreatedAsync();
 
             await CreateApplicationsAsync();
             await CreateScopesAsync();
 
             async Task CreateApplicationsAsync()
             {
-                var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
+                 var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictApplicationManager>();
 
-                if (await manager.FindByClientIdAsync("console") == null)
-                {
-                    await manager.CreateAsync(new OpenIddictApplicationDescriptor
-                    {
-                        ClientId = "console",
-                        ClientSecret = "388D45FA-B36B-4988-BA59-B187D329C207",
-                        DisplayName = "My client application",
-                        Permissions =
-                {
-                    Permissions.Endpoints.Token,
-                    Permissions.GrantTypes.ClientCredentials
-                }
-                    });
-                }
-
-                if (await manager.FindByClientIdAsync("test")==null)
-                {
-                    await manager.CreateAsync(new OpenIddictApplicationDescriptor
-                    {
-                        ClientId = "test",
-                        DisplayName= "test",
-                        RedirectUris= {new Uri("http://localhost")},
-                        Permissions = 
-                        {
-                            Permissions.Endpoints.Authorization,
-                            Permissions.Endpoints.Token,
-                            Permissions.GrantTypes.AuthorizationCode,
-                            Permissions.GrantTypes.ClientCredentials,
-                            Permissions.GrantTypes.RefreshToken,
-                            Permissions.Prefixes.Scope+"api",
-                            Permissions.Prefixes.Scope+"openid",
-                            Permissions.Prefixes.Scope+"offline_access",
-                            Permissions.ResponseTypes.Code
-                        }
-                    });
-                }
-
-                if (await manager.FindByClientIdAsync("console_app") is null)
-                {
-                    await manager.CreateAsync(new OpenIddictApplicationDescriptor
-                    {
-                        ClientId = "console_app",
-                        RedirectUris =
-                        {
-                            new Uri("http://localhost:5213/")
-                        },
-                        Permissions =
-                        {
-                            Permissions.Endpoints.Authorization,
-                            Permissions.Endpoints.Token,
-                            Permissions.GrantTypes.AuthorizationCode,
-                            Permissions.ResponseTypes.Code,
-                            Permissions.Scopes.Email,
-                            Permissions.Scopes.Profile,
-                            Permissions.Scopes.Roles,
-                            Permissions.Prefixes.Scope + "api1",
-                            Permissions.Prefixes.Scope + "api2"
-                        }
-                    });
-                }
-
-                if (await manager.FindByClientIdAsync("resource_server_1") is null)
-                {
-                    await manager.CreateAsync(new OpenIddictApplicationDescriptor
-                    {
-                        ClientId = "resource_server_1",
-                        ClientSecret = "846B62D0-DEF9-4215-A99D-86E6B8DAB342",
-                        Permissions =
-                        {
-                            Permissions.Endpoints.Introspection
-                        }
-                    });
-                }
             }
 
             async Task CreateScopesAsync()
             {
                 var manager = scope.ServiceProvider.GetRequiredService<IOpenIddictScopeManager>();
 
-                if (await manager.FindByNameAsync("api1") is null)
-                {
-                    await manager.CreateAsync(new OpenIddictScopeDescriptor
-                    {
-                        Name = "api1",
-                        Resources =
-                        {
-                            "resource_server_1"
-                        }
-                    });
-                }
-
-                if (await manager.FindByNameAsync("api2") is null)
-                {
-                    await manager.CreateAsync(new OpenIddictScopeDescriptor
-                    {
-                        Name = "api2",
-                        Resources =
-                        {
-                            "resource_server_2"
-                        }
-                    });
-                }
             }
         }
 
